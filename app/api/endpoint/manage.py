e0d9e8702fd0df196b73a869aa399f396787e01c
u20250030017 <http://code.gitlab.gt/side-project/gather/maenggler/docgen/docgen-back-office/-/tree/develop?ref_type=heads>
2025-10-15 15:50:46 +0900

    api 엔드포인트/manage.py에 users_count, places_count 추가, routers.py에 manage 생성성

--- app/api/endpoint/manage.py
+++ app/api/endpoint/manage.py
@@ -1,10 +1,13 @@
 from fastapi import APIRouter, Depends
 from app.repositories import userdb
-from app.db.postgresql import get_db
+from app.db.postgresql import get_db,place_db
 from sqlalchemy.ext.asyncio import AsyncSession
 router = APIRouter()
 
-@router.get("/users/count")
+@router.get("/dashboard")
 async def get_users_count(db: AsyncSession = Depends(get_db)):
     users = await userdb.get_users(db)
-    return {"count": len(users)}
+    users_count = len(users)
+    places = await place_db.get_places(db)
+    places_count = len(places)
+    return {"users_count": users_count, "places_count": places_count}
--- app/api/routers.py
+++ app/api/routers.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from app.api.endpoint import example, auth, map, place
+from app.api.endpoint import example, auth, map, place, manage
 
 api_router = APIRouter()
 
@@ -25,4 +25,10 @@ api_router.include_router(
   map.router,
   prefix='/map',
   tags=["map"]
+)
+
+api_router.include_router(
+  manage.router,
+  prefix='/manage',
+  tags=["manage"]
 )
